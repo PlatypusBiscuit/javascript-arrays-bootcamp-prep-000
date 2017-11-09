8608d8f98281cede888c9f9716385fd13ec6241a
Luke Matheny <lukem17@gmail.com>
2017-11-09 22:49:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,3 +12,7 @@ function addElementToEndOfArray(array, element) {
 function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element);
 }
+
+function accessElementInArray() {
+  
+}
