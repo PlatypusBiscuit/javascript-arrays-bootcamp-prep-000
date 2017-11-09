cb5788bf79ee2b723c6f120286808c598da305b0
Luke Matheny <lukem17@gmail.com>
2017-11-09 22:47:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,7 @@
 var ChocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element) {
-  
+  return [element, ...array];
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array = [element, ...array];
-  return array;
+  array.unshift(element);
 }
